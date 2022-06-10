2a3c1de2d8addb77c1a54e62a2cb7f7eab22ece4
Cecilia <cecilia.perdomo@gmail.com>
2022-06-10 20:38:40 -0300

    commit 1

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    mongoDB: {
+        url: 'mongodb+srv://Cecilia:ceci1984@cluster1.sf6kh.mongodb.net/?retryWrites=true&w=majority',
+        options: {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            //useCreateIndex: true,
+        }
+    }
+}
+module.exports = config
